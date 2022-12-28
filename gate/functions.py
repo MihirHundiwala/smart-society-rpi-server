6d89ab928a6fcfb854f255de0de6568d1eb9ba77
Mihir Hundiwala <mihir.hundiwala@somaiya.edu>
2022-12-28 23:02:14 +0530

    bug fixes

--- gate/functions.py
+++ gate/functions.py
@@ -4,8 +4,9 @@ import RPi.GPIO as GPIO
 def gate_control_function(MQTTClient):
     gate_control_function.stop = False   # Function attribute used for stopping thread
 
-    def blinkled():
+    def blinkled(client, userdata, message):
         # Set pin 36 to be an output pin and set initial value to low (off)
+        print("Opening Gate...")
         GPIO.setup(36, GPIO.OUT)
         for i in range(3):
             GPIO.output(36, GPIO.HIGH)  # Turn on
@@ -14,4 +15,4 @@ def gate_control_function(MQTTClient):
             time.sleep(1)  # Sleep for 1 second
 
     print("Subscribing to topic 'GATE_CONTROL' ...")
-    MQTTClient.subscribe(topic=f"GATE_CONTROL", QoS=0, callback=blinkled)
+    MQTTClient.subscribe(topic="GATE_CONTROL", QoS=0, callback=blinkled)
