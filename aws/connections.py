3d2d6a44fb5387926c96f7ede5726ff4e1bb654f
ansh-dagha <ansh.dagha@somaiya.edu>
2023-02-06 15:21:09 +0530

    plants

--- aws/connections.py
+++ aws/connections.py
@@ -2,7 +2,7 @@ from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 
 MQTTClient = AWSIoTMQTTClient("SmartSocietyRPI") # random key, if another connection using the same key is opened the previous one is auto closed by AWS IOT
 MQTTClient.configureEndpoint("a3tsmej2y71ety-ats.iot.ap-south-1.amazonaws.com", 8883)
-MQTTClient.configureCredentials("/home/pi/AWSIoTCore/root-ca.crt", "/home/pi/AWSIoTCore/private.key", "/home/pi/AWSIoTCore/certificate.pem")
+MQTTClient.configureCredentials("./certs/root-ca.crt", "./certs/private.key", "./certs/certificate.pem")
 MQTTClient.configureOfflinePublishQueueing(-1) # Infinite offline Publish queueing
 MQTTClient.configureDrainingFrequency(2) # Draining: 2 Hz
 MQTTClient.configureConnectDisconnectTimeout(10) # 10 sec
