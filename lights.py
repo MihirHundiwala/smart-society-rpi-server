ecea4b8a2c848c78151ef6937d68763163401894
Mihir Hundiwala <mihir.hundiwala@somaiya.edu>
2023-03-31 15:40:54 +0530

    .

--- lights.py
+++ lights.py
@@ -7,30 +7,21 @@ def light_control_function(MQTTClient, light_config):
     # Function attribute used for stopping thread
     light_control_function.stop = False
 
-    # Pin for taking sensor inputs
-    ldr_pin = int(light_config['ldr_pin'])
-    output_pin = int(light_config['output_pin'])
-    GPIO.setup(output_pin, GPIO.OUT, initial=Mode_To_GPIO_Signal["AUTO"])
-
+    class Mode:
+        mode = "AUTO"
+    
     Mode_To_GPIO_Signal = {
         "ON": GPIO.LOW,
         "OFF": GPIO.HIGH,
-        "AUTO": auto_mode(ldr_pin),
     }
 
-    class Mode:
-        mode = "AUTO"
-
-    def on_light_signal_received(client, userdata, message):
-        nonlocal Mode
-        try:
-            payload = json.loads(message.payload)
-            Mode.mode = payload.get("mode", "AUTO")
-            print(payload.get("message"))
-        except Exception as err:
-            print(f"Payload Object has an error.\nPayload: {payload}\nException Error: {err}")
+    # Pin for taking sensor inputs
+    ldr_pin = int(light_config['ldr_pin'])
+    output_pin = int(light_config['output_pin'])
+    GPIO.setup(output_pin, GPIO.OUT)
 
     def auto_mode(ldr_pin):
+        nonlocal Mode_To_GPIO_Signal
         try:
             count = 0
             GPIO.setup(ldr_pin, GPIO.OUT)
@@ -51,11 +42,23 @@ def light_control_function(MQTTClient, light_config):
             print("Error while receiving input from sensor.\n Exception:", err)
 
         return Mode_To_GPIO_Signal["ON"]
+    
+    def on_light_signal_received(client, userdata, message):
+        nonlocal Mode
+        try:
+            payload = json.loads(message.payload)
+            Mode.mode = payload.get("mode", "AUTO")
+            print(payload.get("message"))
+        except Exception as err:
+            print(f"Payload Object has an error.\nPayload: {payload}\nException Error: {err}")
 
     MQTTClient.subscribe(topic=f"LIGHT_MODE_CONTROL/{light_config['light_id']}", QoS=0, callback=on_light_signal_received)
     print("Subscribed to topic 'LIGHT_MODE_CONTROL' ...")
 
     while not light_control_function.stop:
-        GPIO.output(output_pin, Mode_To_GPIO_Signal[Mode.mode])
+        if Mode.mode == "AUTO":
+            GPIO.output(output_pin, auto_mode(ldr_pin))
+        else:
+            GPIO.output(output_pin, Mode_To_GPIO_Signal[Mode.mode])
 
     print(f"Stopped thread for light-{light_config['light_id']}")
